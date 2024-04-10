328be510e399e0c9901657ec9683086d55bc67b9
Alejandro Morales <AlejandroM@redefectiva.local>
2024-04-10 15:03:37 -0600

    Lambda V1

--- src/index.mjs
+++ src/index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
     // TODO implement
     const response = {
       statusCode: 200,
-      body: JSON.stringify('Hola desde lambda!'),
+      body: JSON.stringify('Hola desde lambda '),
     };
     return response;
   };
